ca63977296c1a073a8bad893d1b3bb53560b9cde
Slava Nidilko <55159182+Murmaid-ns@users.noreply.github.com>
2020-08-02 14:06:51 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,12 +1,13 @@
 "use strict"
+
 for (let i = 0; i < 5; i++) {
     let star = '*';
     for (let j = 0; j < i; j++) {
         star += '*';
     }
-
     console.log(star);
 }
+
 for (let i = 0; i < 4; i++) {
     let star = '*';
     for (let j = 3; j > i; j--) {
