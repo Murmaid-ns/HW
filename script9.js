2e80bd0683f687ee00165c0ca25d09dea21b413d
Slava <19uvs99@gmail.com>
2020-08-16 15:23:13 +0300

    правки дз9

--- script9.js
+++ script9.js
@@ -8,27 +8,54 @@ const userInfo = [
     {name: 'Mark', gender: 'male', yearOfBirth: 1999},
     {name: 'Maria', gender: 'female', yearOfBirth: 1989},
     {name: 'Igor', gender: 'male', yearOfBirth: 2000},
-    {name: 'John', gender: 'male', yearOfBirth: 1995},
+    {name: 'John', gender: 'male', yearOfBirth: 1995}
 ];
 
-function addIndexInOdj(userInfo) {
-    for (let i = 0; i < userInfo.length; i++) {
-        userInfo[i].index = i;
-    }
-    return userInfo;
+userInfo.forEach((element, i) => element.index = i);
+console.log(userInfo);
+
+function genderCount() {
+    const countObj = userInfo.reduce((counter, user) => {
+        const increaseField = user.gender;
+        if (counter.hasOwnProperty(increaseField)) {
+            counter[increaseField]++;
+
+        } else {
+            counter[increaseField] = 1;
+        }
+        return counter;
+    }, {
+        male: 0,
+        female: 0
+    });
+    return countObj.female > countObj.male ? 'female' : 'male';
 }
 
-console.log(addIndexInOdj(userInfo));
 
-const whichGenderIsMore = userInfo.reduce((curGender, prevGender) => {
-    return curGender.gender > prevGender.gender ? curGender : prevGender.gender;
-}, {gender: ''});
+console.log('More is', genderCount(userInfo), 'gender');
 
-console.log('More is', whichGenderIsMore, 'gender');
-//todo: доделать
-// const arr = ['ololo', 'anna', 'red', 'ololo', 'qwe', 'anna', 'ololo'];
-// const newObj = arr.reduce((accumulator, curElement) => {
-//     const element = new Object(curElement);
-//     return element;
-// }, 0);
-// console.log(newObj);
+
+const arr = ['ololo', 'anna', 'red', 'ololo', 'qwe', 'anna', 'ololo'];
+const counterElements = arr.reduce((count, element, index) => {
+    if (count.hasOwnProperty(element)) {
+        count[element]++;
+    } else {
+        count[element] = 1;
+    }
+    return count;
+}, {});
+console.log(counterElements);
+
+
+const user = {
+    name: 'Alex',
+    age: 21,
+};
+
+const reverseUser = Object.entries(user).reduce((reversed, entries) => {
+    const key = entries[0];
+    const value = entries[1];
+    reversed[value] = key;
+    return reversed;
+}, {});
+console.log(reverseUser);
